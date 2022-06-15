9e9eb8e3aa65fd44f0d7c3f41d98272639986cab
Dayne <dayneflorence13@gmail.com>
2022-06-15 22:40:33 +0200

    final connection test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello
+          This is testing github/heroku connection!
         </a>
       </header>
     </div>
